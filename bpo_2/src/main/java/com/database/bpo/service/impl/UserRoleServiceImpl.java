d070f0acc1d391daecc2d32257320fd854550d2b
crazy18 <1099461301@qq.com>
2020-12-24 09:17:12 +0800

    项目发布板块
    项目内用户角色id添加

---
+++ bpo_2/src/main/java/com/database/bpo/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,36 @@
+package com.database.bpo.service.impl;
+
+import com.database.bpo.dao.UserRoleDao;
+import com.database.bpo.pojo.entity.UserRole;
+import com.database.bpo.service.UserRoleService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService {
+    @Resource
+    UserRoleDao dao;
+
+    @Override
+    public boolean addNewUserRole(Integer userId) {
+        UserRole userRole = new UserRole();
+        userRole.setUserId(userId);
+
+        userRole.setRoleId(1);
+        int employer = dao.insert(userRole);
+        userRole.setRoleId(2);
+        int employee = dao.insert(userRole);
+        if(employee != 0 && employer != 0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public UserRole findUserRole(Integer userId){
+        UserRole userRole = new UserRole();
+        return userRole;
+    }
+
+}
