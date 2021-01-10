2d05d69b022ffd683beeddba2b8dd825eff386b7
crazy18 <1099461301@qq.com>
2021-01-10 23:49:45 +0800

    服务器部署修改

--- bpo_2/src/test/java/com/database/bpo/BpoApplicationTest.java
+++ bpo_2/src/test/java/com/database/bpo/BpoApplicationTest.java
@@ -35,42 +35,42 @@ public class BpoApplicationTest {
 
     @Test
     void CreateNewNotification(){
-        MoneyNotification notification = new MoneyNotification();
-        Integer orderId = 9;
-        Float amount = null;
-        String moneyDescription = null;
-        Integer userRoleId = 1;
-//        如果是发布方支付定金
-        if (userRoleId == 1){
-            amount = (float) 1000;
-            moneyDescription = "尝试" + "发布方定金";
-        }
-        notification.setOrderId(orderId);
-        notification.setMoneyTime(new Date());
-        notification.setAmount(amount);
-        notification.setMoneyType("支付定金");
-        notification.setUserId(1);
-        notification.setMoneyDescription(moneyDescription);
-//        moneyNotificationDao.insert(notification);
-        billService.AddNewBill(notification);
-        userService.ChangeMoney(notification);
-        cashFlowService.AddNewCashFlow(notification);
+//        MoneyNotification notification = new MoneyNotification();
+//        Integer orderId = 9;
+//        Float amount = null;
+//        String moneyDescription = null;
+//        Integer userRoleId = 1;
+////        如果是发布方支付定金
+//        if (userRoleId == 1){
+//            amount = (float) 1000;
+//            moneyDescription = "尝试" + "发布方定金";
+//        }
+//        notification.setOrderId(orderId);
+//        notification.setMoneyTime(new Date());
+//        notification.setAmount(amount);
+//        notification.setMoneyType("支付定金");
+//        notification.setUserId(1);
+//        notification.setMoneyDescription(moneyDescription);
+////        moneyNotificationDao.insert(notification);
+//        billService.AddNewBill(notification);
+//        userService.ChangeMoney(notification);
+//        cashFlowService.AddNewCashFlow(notification);
     }
 
     @Test
     void TestMoneyController(){
-        Integer userRoleId = 1;
-        Integer userId = 1;
-        Orderwithcontact order = orderService.SelectByOrderId(9);
-//        添加拨款单
-        MoneyNotification notification = moneyService.CreateDepositNotification(order, userId,userRoleId);
-//        用户账单 添加支付定金记录
-        billService.AddNewBill(notification);
-//        修改用户账户余额
-        userService.ChangeMoney(notification);
-//        平台流水表 添加定金收款记录
-        cashFlowService.AddNewCashFlow(notification);
-//        订单修改状态
-        orderService.DepositChangeOrderState(order,userRoleId);
+//        Integer userRoleId = 1;
+//        Integer userId = 1;
+//        Orderwithcontact order = orderService.SelectByOrderId(9);
+////        添加拨款单
+//        MoneyNotification notification = moneyService.CreateDepositNotification(order, userId,userRoleId);
+////        用户账单 添加支付定金记录
+//        billService.AddNewBill(notification);
+////        修改用户账户余额
+//        userService.ChangeMoney(notification);
+////        平台流水表 添加定金收款记录
+//        cashFlowService.AddNewCashFlow(notification);
+////        订单修改状态
+//        orderService.DepositChangeOrderState(order,userRoleId);
     }
 }
